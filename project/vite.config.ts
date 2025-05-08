ca4b8b5ffd46808e34a491759f6ce3f767d78049
q0uackie <tobias.oberegger2008@gmail.com>
2025-05-08 11:53:38 +0200

    vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Wichtig für relative Pfade
+  server: {
+    host: true // Macht die Seite im Netzwerk erreichbar
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
